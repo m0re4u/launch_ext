3a81bd244b1b56e11733826ad43836a3e8292739
Michiel van der Meer <michielmeer@live.nl>
2016-02-02 18:35:03 +0100

    working on links open in new tab

--- req_load.js
+++ req_load.js
@@ -1,17 +1,17 @@
-httpGetAsync()
+httpGetAsync();
 
 function httpGetAsync()
 {
-    console.log("sending request")
+    console.log("sending request");
     var url = "https://launchlibrary.net/1.1/launch/next/5";
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            var data = JSON.parse(xmlHttp.responseText)
+            var data = JSON.parse(xmlHttp.responseText);
             if (data)
             {
-                document.getElementById("id01").innerHTML = "<b>Loaded " + data.count + " launches:</b>"
-                printLaunches(data.launches)
+                document.getElementById("id01").innerHTML = "<b>Loaded " + data.count + " launches:</b>";
+                printLaunches(data.launches);
             }
     }
     xmlHttp.open("GET", url, true); // true for asynchronous
@@ -22,8 +22,24 @@ function printLaunches(launchArray)
 {
     for(var i = 0; i < launchArray.length; i++)
     {
-        console.log(launchArray[i])
-        document.getElementById("launch" + i).innerHTML = launchArray[i].name
-        document.getElementById("wlaunch" + i).innerHTML = "Window start: " + launchArray[i].windowstart
+        lobj = launchArray[i];
+        console.log(lobj);
+        document.getElementById("launch" + i).innerHTML = lobj.name;
+        if (lobj.vidURL != null)
+        {
+            document.getElementById("wlaunch" + i).innerHTML = "Window start: <a id=\"lbutton" + i + "\"href=\"" + lobj.vidURL + "\">" + lobj.windowstart + "</a>";
+            document.getElementById("wlaunch" + i).innerHTML.replace(' ', '/');  // #NoProblemo
+            console.log(lobj.vidURL);
+            document.getElementById("lbutton" + i).onclick = function(){
+                webcastClicker(i);
+            };
+        } else {
+            document.getElementById("wlaunch" + i).innerHTML = "Window start: " + lobj.windowstart;
+        }
     }
+}
+
+function webcastClicker(i)
+{
+    console.log(i)
 }
