98b531755ab97d64aaa8490b2fd6b0fd017de2ec
Michiel <michielmeer@live.nl>
2016-02-03 14:13:26 +0100

    updated clickable webcast links

--- req_load.js
+++ req_load.js
@@ -1,5 +1,7 @@
 httpGetAsync();
 
+REQ_SIZE = 5;
+
 function httpGetAsync()
 {
     console.log("sending request");
@@ -27,19 +29,20 @@ function printLaunches(launchArray)
         document.getElementById("launch" + i).innerHTML = lobj.name;
         if (lobj.vidURL != null)
         {
-            document.getElementById("wlaunch" + i).innerHTML = "Window start: <a id=\"lbutton" + i + "\"href=\"" + lobj.vidURL + "\">" + lobj.windowstart + "</a>";
+            document.getElementById("wlaunch" + i).innerHTML = "Window start: <a id=\"lbutton" + i + "\">" + lobj.windowstart + "</a>";
+            url = addHTTP(lobj.vidURL) 
+            document.getElementById("lbutton" + i).href = url
+            document.getElementById("lbutton" + i).target = "_blank"
             document.getElementById("wlaunch" + i).innerHTML.replace(' ', '/');  // #NoProblemo
-            console.log(lobj.vidURL);
-            document.getElementById("lbutton" + i).onclick = function(){
-                webcastClicker(i);
-            };
         } else {
             document.getElementById("wlaunch" + i).innerHTML = "Window start: " + lobj.windowstart;
         }
     }
 }
 
-function webcastClicker(i)
-{
-    console.log(i)
+function addHTTP(url) {
+   if (!/^(f|ht)tps?:\/\//i.test(url)) {
+      url = "http://" + url;
+   }
+   return url;
 }
