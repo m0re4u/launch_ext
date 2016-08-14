2901335539eada9957993ca903cacd7e04356fcd
Michiel <michielmeer@live.nl>
2016-08-14 17:24:44 +0200

    now shows a TBD instead of nonexistent launch times

--- req_load.js
+++ req_load.js
@@ -25,19 +25,30 @@ function printLaunches(launchArray)
     {
         var lobj = launchArray[i];
         console.log(lobj);
-        var deadline = lobj.windowstart;
+        var deadline = 0;
+        var deadline_str = "";
+        // Check if a net time is known for the launch. If this is not
+        // available, show a TBD.
+        if (lobj.netstamp != 0){
+            deadline = lobj.netstamp;
+            deadline_str = "T-0: <a id=\"lbutton" + i + "\">" + lobj.net;
+        } else {
+            deadline_str = "TBD";
+        }
+        // Print the name of the launch
         document.getElementById("zlaunch" + i).innerHTML = lobj.name;
+
+        // Get a link to stream if available
         if (lobj.vidURL != null && lobj.vidURL != "")
         {
-            // Get a link to stream if available
-            document.getElementById("wlaunch" + i).innerHTML = "Window start: <a id=\"lbutton" + i + "\">" + deadline + "</a> | <span id=\"cdown" + i + "\"></span>";
+            document.getElementById("wlaunch" + i).innerHTML = deadline_str + "</a><span id=\"cdown" + i + "\"></span>";
             url = addHTTP(lobj.vidURL)
             document.getElementById("lbutton" + i).href = url
             document.getElementById("lbutton" + i).target = "_blank"
             document.getElementById("wlaunch" + i).innerHTML.replace(' ', '/');  // #NoProblemo
             getTimeRemaining(deadline, i);
         } else {
-            document.getElementById("wlaunch" + i).innerHTML = "Window start: <span id=\"lbutton" + i + "\">" + deadline + "</span> | <span id=\"cdown" + i + "\"></span>";
+            document.getElementById("wlaunch" + i).innerHTML = deadline_str + "</span><span id=\"cdown" + i + "\"></span>";
             getTimeRemaining(deadline, i);
         }
     }
@@ -51,20 +62,32 @@ function addHTTP(url) {
    return url;
 }
 
+/**
+ * Get the time remainig until the timestamp given and write it to the
+ * countdown cell. However, if the timestamp is 0, no time will be written back.
+ *
+ * @param  {int} endtime UNIX timestamp
+ * @param  {int} i       index in launch array
+ *
+ */
 function getTimeRemaining(endtime, i){
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor( (t/1000) % 60 );
-  var minutes = Math.floor( (t/1000/60) % 60 );
-  var hours = Math.floor( (t/(1000*60*60)));
-  // Prepend 0 to numbers lower than 10
-  seconds = ('0' + seconds).slice(-2)
-  minutes = ('0' + minutes).slice(-2)
-  var clockString = "" + hours + ":" + minutes + ":" + seconds
-  document.getElementById("cdown" + i).innerHTML = clockString;
+    if (endtime == 0){
+        document.getElementById("cdown" + i).innerHTML = "";
+    } else {
+        var t = endtime*1000 - Date.parse(new Date());
+        var seconds = Math.floor( (t/1000) % 60 );
+        var minutes = Math.floor( (t/1000/60) % 60 );
+        var hours = Math.floor( (t/(1000*60*60)));
+        // Prepend 0 to numbers lower than 10
+        seconds = ('0' + seconds).slice(-2)
+        minutes = ('0' + minutes).slice(-2)
+        var clockString = "" + hours + ":" + minutes + ":" + seconds
+        document.getElementById("cdown" + i).innerHTML = clockString;
+    }
 }
 
 function updateCountdown(launchArray){
     for (var i = 0; i < launchArray.length; i++) {
-        getTimeRemaining(launchArray[i].windowstart, i);
+        getTimeRemaining(launchArray[i].netstamp, i);
     }
 }
