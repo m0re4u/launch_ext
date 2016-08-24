bcd01f6ce62590b538722d5e2372dc265e85d888
Michiel <michielmeer@live.nl>
2016-08-24 19:26:31 +0200

    No flag shown if country code is unknown

--- req_load.js
+++ req_load.js
@@ -1,21 +1,21 @@
+'use strict';
 var countryConvert = {"USA" : "us", "FRA":"europeanunion", "IND":"in", "CHN" : "cn"}
 
 httpGetAsync();
 
 function httpGetAsync()
 {
-    console.log("sending request");
     var url = "https://launchlibrary.net/1.1/launch/next/5";
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
             var data = JSON.parse(xmlHttp.responseText);
-            if (data)
-            {
-                document.getElementById("launch_header").innerHTML = "<b>Loaded " + data.count + " launches:</b>";
-                printLaunches(data.launches);
-                setInterval(function(){updateCountdown(data.launches);}, 1000)
-            }
+        if (data)
+        {
+            document.getElementById("launch_header").innerHTML = "<b>Loaded " + data.count + " launches:</b>";
+            printLaunches(data.launches);
+            setInterval(function(){updateCountdown(data.launches);}, 1000)
+        }
     }
     xmlHttp.open("GET", url, true); // true for asynchronous
     xmlHttp.send(null);
@@ -26,7 +26,7 @@ function printLaunches(launchArray)
     for(var i = 0; i < launchArray.length; i++)
     {
         var lobj = launchArray[i];
-        console.log(lobj);
+        // console.log(lobj);
         var deadline = 0;
         var deadline_str = "";
         // Check if a net time is known for the launch. If this is not
@@ -37,16 +37,16 @@ function printLaunches(launchArray)
         } else {
             deadline_str = "TBD";
         }
-        // Print the name of the launch
+        // Print country flag and the name of the launch
         document.getElementById("zlaunch" + i).innerHTML = insertCountryFlag(lobj.location.countryCode) + "&ensp;" + lobj.name;
 
         // Get a link to stream if available
         if (lobj.vidURL != null && lobj.vidURL != "")
         {
             document.getElementById("wlaunch" + i).innerHTML = deadline_str + "</a><span id=\"cdown" + i + "\"></span>";
-            url = addHTTP(lobj.vidURL)
-            document.getElementById("lbutton" + i).href = url
-            document.getElementById("lbutton" + i).target = "_blank"
+            var url = addHTTP(lobj.vidURL);
+            document.getElementById("lbutton" + i).href = url;
+            document.getElementById("lbutton" + i).target = "_blank";
             document.getElementById("wlaunch" + i).innerHTML.replace(' ', '/');  // #NoProblemo
             getTimeRemaining(deadline, i);
         } else {
@@ -81,9 +81,9 @@ function getTimeRemaining(endtime, i){
         var minutes = Math.floor( (t/1000/60) % 60 );
         var hours = Math.floor( (t/(1000*60*60)));
         // Prepend 0 to numbers lower than 10
-        seconds = ('0' + seconds).slice(-2)
-        minutes = ('0' + minutes).slice(-2)
-        var clockString = "" + hours + ":" + minutes + ":" + seconds
+        seconds = ('0' + seconds).slice(-2);
+        minutes = ('0' + minutes).slice(-2);
+        var clockString = "" + hours + ":" + minutes + ":" + seconds;
         document.getElementById("cdown" + i).innerHTML = clockString;
     }
 }
@@ -96,6 +96,8 @@ function updateCountdown(launchArray){
 
 function insertCountryFlag(countryAbbrev){
     if(countryAbbrev in countryConvert){
-        return "<img src=\"flags/" + countryConvert[countryAbbrev] + ".png\">"
+        return "<img src=\"flags/" + countryConvert[countryAbbrev] + ".png\">";
+    } else {
+        return "";
     }
 }
